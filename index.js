546502e2840d447fbb30fddd042620f75f61dc51
Zach Wagner <zach.wagner@spothero.com>
2019-03-08 17:38:44 -0600

    add what section text

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ var frameworks = require("./data/frameworks");
 var languages = require("./data/languages");
 var resume = require("./data/resume");
 
+var whatDescStr = "Like I said above, I have skills across the full tech stack but have developed a " +
+    "passion for front-end.  There's nothing quite like the gratification of seeing your creations in the " +
+    "browser and out on the web!";
+
 module.exports = {
     logos: images.logos,
     profileImage: images.profile,
     companies: companies,
     frameworks: frameworks,
     languages: languages,
-    resume: resume
+    resume: resume,
+    whatSectionDescription: whatDescStr
 };
