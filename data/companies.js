484a65ba7d421945664df95da8823d304754d9b2
Zach Wagner <zach.wagner@spothero.com>
2019-03-10 15:46:41 -0500

    update company keys

--- data/companies.js
+++ data/companies.js
@@ -2,16 +2,16 @@ var companies = [
     {
         name: "SpotHero",
         location: "Chicago, IL",
-        from: "December 2016",
-        to: "Present",
+        fromDate: "December 2016",
+        toDate: "Present",
         title: "Front End Engineer",
         url: "https://www.spothero.com"
     },
     {
         name: "Slalom Consulting",
         location: "Chicago, IL",
-        from: "December 2012",
-        to: "December 2016",
+        fromDate: "December 2012",
+        toDate: "December 2016",
         title: "Senior Software Engineer",
         url: "https://www.slalom.com/locations/chicago"
     }
